77eb3ec6ef9dcdde4a5461a2d83ef54470ecffc8
unknown <freelancerfathe@gmail.com>
2022-02-20 08:28:31 +0600

    da

--- app.js
+++ app.js
@@ -75,6 +75,10 @@ async function run() {
 
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
