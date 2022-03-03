9706cfea7a364c8b33211c6044054060ba0ddcf8
unknown <freelancerfathe@gmail.com>
2022-03-03 17:13:37 +0600

    sd

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const shortId = require('shortid')
 
 
 
-const uri = `mongodb+srv://bicyledbnew:bicyledbnew321@cluster0.wanl6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wanl6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(port, () => {
